e417c3baf4ddb0dd87575eaceb79892a494a936a
Stephan Fuhrmann <s@sfuhrm.de>
2025-06-01 12:13:43 +0200

    Harmonize then statemens Check readability of USERNAME_FILE and PASSWORD_FILE

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,27 @@
 set -e
 MOUNTPOINT=/media/data/
 
-if [ ! -d "/etc/nginx/http.d" ]
-then
+if [ ! -d "/etc/nginx/http.d" ]; then
 	echo "Could not find http.d config dir, exiting!"
 	exit 1
 fi
-if [ ! -d "$MOUNTPOINT" ]
-then
+if [ ! -d "$MOUNTPOINT" ]; then
 	echo "Could not find data $MOUNTPOINT dir, exiting!"
 	exit 2
 fi
-if [ ! -r "$MOUNTPOINT" ]
-then
+if [ ! -r "$MOUNTPOINT" ]; then
 	echo "Could read-access data $MOUNTPOINT dir, exiting!"
 	exit 3
 fi
 
 if [ -n "$USERNAME_FILE" ] && [ -n "$PASSWORD_FILE" ]; then
-	echo "Username / password taken from files."
-	htpasswd -bc /etc/nginx/htpasswd "$(cat $USERNAME_FILE)" "$(cat $PASSWORD_FILE)"
+	if [ -r "$USERNAME_FILE" ] && [ -r "$PASSWORD_FILE" ]; then
+		echo "Username / password taken from files."
+		htpasswd -bc /etc/nginx/htpasswd "$(cat $USERNAME_FILE)" "$(cat $PASSWORD_FILE)"
+	else
+		echo "Files $USERNAME_FILE and/or $PASSWORD_FILE are not readable!"
+		exit 5
+	fi
 elif [ -n "$USERNAME" ] && [ -n "$PASSWORD" ]; then
 	echo "Username / password taken from env."
 	htpasswd -bc /etc/nginx/htpasswd "$USERNAME" "$PASSWORD"
