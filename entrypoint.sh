4990dd1c7106597d1f4b3f2caaffd7256f43ffa4
Stephan Fuhrmann <s@sfuhrm.de>
2025-10-03 15:15:53 +0200

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ fi
 if [ -n "$USERNAME_FILE" ] && [ -n "$PASSWORD_FILE" ]; then
 	if [ -r "$USERNAME_FILE" ] && [ -r "$PASSWORD_FILE" ]; then
 		echo "Username / password taken from files."
-		htpasswd < "$PASSWORD_FILE" -ic /etc/nginx/htpasswd "$(cat $USERNAME_FILE)"
+		htpasswd < "$PASSWORD_FILE" -ic /etc/nginx/permissive/htpasswd "$(cat $USERNAME_FILE)"
 	else
 		echo "Files $USERNAME_FILE and/or $PASSWORD_FILE are not readable!"
 		exit 4
@@ -30,7 +30,7 @@ elif [ -n "$USERNAME" ] && [ -n "$PASSWORD" ]; then
 else
     echo Using no auth.
 	sed -i 's%auth_basic "Restricted";% %g' /etc/nginx/permissive/default.conf
-	sed -i 's%auth_basic_user_file /etc/nginx/htpasswd;% %g' /etc/nginx/permissive/default.conf
+	sed -i 's%auth_basic_user_file /etc/nginx/permissive/htpasswd;% %g' /etc/nginx/permissive/default.conf
 fi
 
 exec "$@"
