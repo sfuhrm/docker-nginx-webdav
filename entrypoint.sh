b25cd9f3cdb622500b18a10718b516f2c48340be
Stephan Fuhrmann <s@sfuhrm.de>
2026-02-05 22:04:40 +0100

    Idempotency for config files More secure var usage No passwords in ps (?)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 MOUNTPOINT=/media/data/
+CONFIG_FILE="/etc/nginx/http.d/default.conf"
+CONFIG_BACKUP="/etc/nginx/http.d/default.conf.bak"
 
 if [ ! -d "/etc/nginx/http.d" ]; then
 	echo "Could not find http.d config dir, exiting!"
@@ -16,21 +18,28 @@ if [ ! -r "$MOUNTPOINT" ]; then
 	exit 3
 fi
 
+# Restore original config if it exists, or create backup to ensure idempotency
+if [ -f "$CONFIG_BACKUP" ]; then
+    cp "$CONFIG_BACKUP" "$CONFIG_FILE"
+else
+    cp "$CONFIG_FILE" "$CONFIG_BACKUP"
+fi
+
 if [ -n "$USERNAME_FILE" ] && [ -n "$PASSWORD_FILE" ]; then
 	if [ -r "$USERNAME_FILE" ] && [ -r "$PASSWORD_FILE" ]; then
 		echo "Username / password taken from files."
-		htpasswd < "$PASSWORD_FILE" -ic /etc/nginx/htpasswd "$(cat $USERNAME_FILE)"
+		htpasswd -ic /etc/nginx/htpasswd "$(cat "$USERNAME_FILE")" < "$PASSWORD_FILE"
 	else
 		echo "Files $USERNAME_FILE and/or $PASSWORD_FILE are not readable!"
 		exit 4
 	fi
 elif [ -n "$USERNAME" ] && [ -n "$PASSWORD" ]; then
 	echo "Username / password taken from env."
-	htpasswd -bc /etc/nginx/htpasswd "$USERNAME" "$PASSWORD"
+	echo "$PASSWORD" | htpasswd -ic /etc/nginx/htpasswd "$USERNAME"
 else
-    echo Using no auth.
-	sed -i 's%auth_basic "Restricted";% %g' /etc/nginx/http.d/default.conf
-	sed -i 's%auth_basic_user_file /etc/nginx/htpasswd;% %g' /etc/nginx/http.d/default.conf
+    echo "Using no auth."
+	sed -i 's%auth_basic "Restricted";% %g' "$CONFIG_FILE"
+	sed -i 's%auth_basic_user_file /etc/nginx/htpasswd;% %g' "$CONFIG_FILE"
 fi
 
 exec "$@"
